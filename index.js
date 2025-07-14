8f0f2e3b69c129df0122d47a8a3c3b9057328c41
David McCullough <dmccullough488@gmail.com>
2025-07-13 23:14:45 -0500

    Added the 2nd modal message after 1.5sec delay

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function () {
   modal.style.display = 'inline'
@@ -11,5 +13,13 @@ modalCloseBtn.addEventListener('click', function () {
 
 consentForm.addEventListener('submit', function (e) {
   e.preventDefault()
-  console.log('form submitted')
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+
+  setTimeout(() => document.getElementById('upload-text').innerText = "Making the sale...", 1500)
 })
